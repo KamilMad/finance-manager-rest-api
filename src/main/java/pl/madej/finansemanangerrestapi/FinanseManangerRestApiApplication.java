c599f382d4415348d391c62620a9d1854f658903
KamilMad <kamilul085@gmail.com>
2024-02-06 18:38:39 +0100

    initial commit

---
+++ src/main/java/pl/madej/finansemanangerrestapi/FinanseManangerRestApiApplication.java
@@ -0,0 +1,13 @@
+package pl.madej.finansemanangerrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanseManangerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanseManangerRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/madej/finansemanangerrestapi/FinanseManangerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.madej.finansemanangerrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanseManangerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
