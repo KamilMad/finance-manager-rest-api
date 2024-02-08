301e8b27bb41512c90fe2e5460c37d23167b65ca
KamilMad <kamilul085@gmail.com>
2024-02-08 21:11:15 +0100

    added jwt dependencies, JwtAuthenticationFilter and JwtService

---
+++ src/main/java/pl/madej/finansemanangerrestapi/security/JwtService.java
@@ -0,0 +1,11 @@
+package pl.madej.finansemanangerrestapi.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    private final String secretKey = "MWE4ZTYwNTY0MjBlNWVmODMyMmY4ZWQ0YzFkODlmODllYWI5ZDFhNDVjNDg1N2IzZTU0YjEwYjUzZDZjYjM1ZA==";
+
+    private Claims
+}
