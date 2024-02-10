07094b69d6465d1f1089e8a598e960f6f98361fa
KamilMad <kamilul085@gmail.com>
2024-02-10 18:29:52 +0100

    added JwtService

--- src/main/java/pl/madej/finansemanangerrestapi/security/JwtService.java
+++ src/main/java/pl/madej/finansemanangerrestapi/security/JwtService.java
@@ -1,11 +1,35 @@
 package pl.madej.finansemanangerrestapi.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private final String secretKey = "MWE4ZTYwNTY0MjBlNWVmODMyMmY4ZWQ0YzFkODlmODllYWI5ZDFhNDVjNDg1N2IzZTU0YjEwYjUzZDZjYjM1ZA==";
 
-    private Claims
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
