98a56f1a3d8878cff3c99a1aa01ade7301e6fb90
KamilMad <kamilul085@gmail.com>
2024-02-10 18:35:36 +0100

    added findAllTransactions endpoint

--- src/main/java/pl/madej/finansemanangerrestapi/controller/TransactionController.java
+++ src/main/java/pl/madej/finansemanangerrestapi/controller/TransactionController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.madej.finansemanangerrestapi.payload.TransactionResponse;
 import pl.madej.finansemanangerrestapi.service.TransactionService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/transaction")
 @RequiredArgsConstructor
@@ -18,8 +20,15 @@ public class TransactionController {
 
     @GetMapping("/{transactionId}")
     public ResponseEntity<TransactionResponse> findTransaction(@PathVariable Long transactionId) {
-        TransactionResponse response = transactionService.getTransaction(transactionId);
 
+        TransactionResponse response = transactionService.getTransaction(transactionId);
         return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponse>> findAllTransactions() {
+
+        List<TransactionResponse> transactions = transactionService.getAllTransactions();
+        return ResponseEntity.ok().body(transactions);
     }
 }
