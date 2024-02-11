6b49d1fdede73449679acd4bf258ef2f32109cd3
KamilMad <kamilul085@gmail.com>
2024-02-11 22:11:42 +0100

    added extractUsername and generateToken methods to JwtService

--- src/main/java/pl/madej/finansemanangerrestapi/security/JwtService.java
+++ src/main/java/pl/madej/finansemanangerrestapi/security/JwtService.java
@@ -2,11 +2,17 @@ package pl.madej.finansemanangerrestapi.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -14,11 +20,32 @@ public class JwtService {
 
     private final String secretKey = "MWE4ZTYwNTY0MjBlNWVmODMyMmY4ZWQ0YzFkODlmODllYWI5ZDFhNDVjNDg1N2IzZTU0YjEwYjUzZDZjYjM1ZA==";
 
+    public String extractUsername(String token) {
+        return extractClaim(token, claims -> claims.getSubject());
+    }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
+    public String generateToken(UserDetails user) {
+        return generateToken(new HashMap<>(), user);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails user
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 *60 *24))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
