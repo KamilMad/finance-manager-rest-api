bc32a3fbecd8400f7037fc15f9c3f53745ad166e
KamilMad <kamilul085@gmail.com>
2024-02-13 20:30:44 +0100

    added endpoints for Investment CRUD operations

---
+++ src/main/java/pl/madej/finansemanangerrestapi/controller/InvestmentController.java
@@ -0,0 +1,54 @@
+package pl.madej.finansemanangerrestapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.madej.finansemanangerrestapi.payload.investment.InvestmentRequest;
+import pl.madej.finansemanangerrestapi.payload.investment.InvestmentResponse;
+import pl.madej.finansemanangerrestapi.service.InvestmentService;
+
+import javax.crypto.spec.IvParameterSpec;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/investment")
+@RequiredArgsConstructor
+public class InvestmentController {
+
+    private final InvestmentService investmentService;
+
+    @GetMapping("/{investmentId}")
+    public ResponseEntity<InvestmentResponse> findInvestment(@PathVariable Long investmentId) {
+
+        InvestmentResponse investmentResponse = investmentService.getInvestment(investmentId);
+        return ResponseEntity.ok(investmentResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<InvestmentResponse>> findAllInvestment() {
+        List<InvestmentResponse> investments = investmentService.getAllInvestment();
+
+        return ResponseEntity.ok(investments);
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> addInvestment(@RequestBody InvestmentRequest investmentRequest) {
+        Long investmentId = investmentService.addInvestment(investmentRequest);
+        return ResponseEntity.ok(investmentId);
+    }
+
+    @DeleteMapping("/{investmentId}")
+    public ResponseEntity<Void> deleteInvestment(@PathVariable Long investmentId) {
+        investmentService.deleteInvestment(investmentId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{investmentId}")
+    public ResponseEntity<InvestmentResponse> updateInvestment(@PathVariable Long investmentId, @RequestBody InvestmentRequest investmentRequest) {
+        InvestmentResponse investmentResponse = investmentService.updateInvestment(investmentId, investmentRequest);
+
+        return ResponseEntity.ok(investmentResponse);
+    }
+
+}
